83d442945332b3e1130faf77593a77973e765c15
Vania Gomes - Bora Codar <vania.vgomes@mercadolivre.com>
2024-02-17 21:04:05 -0300

    Starting Fronten Admin-CodeFix project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import { ThemeProvider } from "@mui/system";
+import "./App.css";
+
+function App() {
+  return <ThemeProvider theme={{}}></ThemeProvider>;
+}
+
+export default App;
