efbae215a454281a885bcfbb5fc66bb97646b089
Vania Gomes - Bora Codar <vania.vgomes@mercadolivre.com>
2024-02-24 21:04:48 -0300

    Create Page component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import * as React from "react";
 
 import { ThemeProvider } from "@mui/system";
 import "./App.css";
+import Home from "./app/page";
 
 function App() {
-  return <ThemeProvider theme={{}}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={{}}>
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
